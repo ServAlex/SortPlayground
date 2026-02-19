40a5520cff67b079dada46c1e69d0babb4530435
alex <servalex1@gmail.com>
2026-02-19 02:56:40 +0100

    remover respects keep chunks option now

--- FileDeletion/LeftoversRemover.cs
+++ FileDeletion/LeftoversRemover.cs
@@ -14,6 +14,13 @@ public class LeftoversRemover
 
 	public void Remove()
 	{
+		var chunksDirectoryPath = Path.Combine(PathOptions.FilesLocation, PathOptions.ChunksDirectoryBaseName);
+		if ((!PathOptions.KeepChunks || PathOptions.DeleteAllCreatedFiles) && Directory.Exists(chunksDirectoryPath))
+		{
+			Console.WriteLine($"Deleted chunks directory at {chunksDirectoryPath}");
+			Directory.Delete(chunksDirectoryPath, true);
+		}
+		
 		if (!PathOptions.DeleteAllCreatedFiles)
 		{
 			return;
@@ -22,21 +29,15 @@ public class LeftoversRemover
 		var unsortedFilePath = Path.Combine(PathOptions.FilesLocation, PathOptions.UnsortedFileName);
 		if (File.Exists(unsortedFilePath))
 		{
+			Console.WriteLine($"Deleted unsorted file at {unsortedFilePath}");
 			File.Delete(unsortedFilePath);
 		}
 			
 		var sortedFilePath = Path.Combine(PathOptions.FilesLocation, PathOptions.SortedFileName);
 		if (File.Exists(sortedFilePath))
 		{
+			Console.WriteLine($"Deleted sorted file at {sortedFilePath}");
 			File.Delete(sortedFilePath);
 		}
-			
-		var chunksDirectoryPath = Path.Combine(PathOptions.FilesLocation, PathOptions.ChunksDirectoryBaseName);
-		if (Directory.Exists(chunksDirectoryPath))
-		{
-			Directory.Delete(chunksDirectoryPath, true);
-		}
-			
-		Console.WriteLine($"Delete flag enabled, deleted unsorted file, chunks directory and sorted file at {PathOptions.FilesLocation}");
 	}
 }
