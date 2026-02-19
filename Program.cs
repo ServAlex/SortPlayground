71ff8898fd2c0a905192abb3ce570a01d7fdfc7e
alex <servalex1@gmail.com>
2026-02-19 02:05:49 +0100

    program cleanup

--- Program.cs
+++ Program.cs
@@ -8,25 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = new HostApplicationBuilder(new HostApplicationBuilderSettings()
-{
-	DisableDefaults = true,
-	ContentRootPath = Directory.GetCurrentDirectory(),
-	Args = args
-});
-//var builder = Host.CreateApplicationBuilder(args);
-//Console.WriteLine(builder.Configuration.GetDebugView());
-
-builder.Configuration.AddJsonFile("appsettings.json", optional:false, reloadOnChange:false);
+var builder = Host.CreateApplicationBuilder(args);
 builder.Configuration.AddCommandLine(args, OptionsHelper.GetSwitchMappings());
-
-// builder.Configuration.AddInMemoryCollection(new List<KeyValuePair<string, string?>>
-// {
-// 	new( $"{nameof(SortOptions)}:{nameof(LargeFileSorterOptions.MemoryBudgetGb)}", "6")
-// });
-
-Console.WriteLine(builder.Configuration.GetDebugView());
-//Console.WriteLine(string.Join(',', args));
+//Console.WriteLine(builder.Configuration.GetDebugView());
 
 builder.Services.AddTransient<FileGenerator>();
 builder.Services.AddTransient<LargeFileSorter>();
